c8f633812a151d83619467a803855ab59b3994cd
JuanDeDiossx <juanswordx@gmail.com>
2025-03-26 09:23:47 -0700

    Limite de 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() =>{
+    if(count > 10){
+      
+      alert("El limite es 10");
+      setCount(0);
+    }
+
+  } , [count]);
+
+  
+
+  
 
   return (
     <>
@@ -25,6 +38,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
