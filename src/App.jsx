a8bbfd2bc9bd85f43c96ecf130a976ce80d44680
JuanDeTeo <juandediosmendozapeinado3@gmail.com>
2025-03-28 14:38:11 -0700

    Tabla de usuario

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-
+//xdXd
 function App() {
   const [count, setCount] = useState(0)
 
