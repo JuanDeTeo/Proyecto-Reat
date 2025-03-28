f3bbe0f3b576972862da7903f4e01d72ce2fbb06
JuanDeDiossx <juanswordx@gmail.com>
2025-03-28 14:21:03 -0700

    Se agrego un cuadro con inf

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,11 +23,34 @@ function App() {
 
   } , [count]);
 
-  
+  const listaUsuario =[{
+    nombre: "Erick",
+    apellido: "Sanchez",
+    edad: 35,
+  },
+  {
+    nombre: "Luis",
+    apellido: "Perez",
+    edad: 23,
+  },
+  {
+    nombre: "Pancho",
+    apellido: "Panteras",
+    edad: 44,
+  },
+  {
+    nombre: "Megumin",
+    apellido: "Cramesi",
+    edad: 14,
+  },
+  {
+    nombre: "Kasuma",
+    apellido: "Basuma",
+    edad: 17,
+  }
+];
 
-  
-
-  return (
+return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -38,6 +69,29 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">nombre</TableCell>
+            <TableCell align="right">apellido</TableCell>
+            <TableCell align="right">edad</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {listaUsuario.map((usuario) => (
+            <TableRow
+              key={usuario.nombre}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="right">{usuario.nombre}</TableCell>
+              <TableCell align="right">{usuario.apellido}</TableCell>
+              <TableCell align="right">{usuario.edad}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
       
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
